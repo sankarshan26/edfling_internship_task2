e19af3247f482c9705f9c265cea9e1c3da32c1d8
Sankarshan Mishra <100289130+sankarshan26@users.noreply.github.com>
2023-08-17 11:14:35 +0530

    navbar being updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,18 +12,34 @@ const Navbar = () => {
     setOpen(!isopen);
   };
 
-  let login = false;
+ const [login, setLogin] = useState(true);
+ function log_status(){
+  setLogin(!login);
+ }
 
+const [pop_up, set_pop_up] = useState("");
+function hadler_popup(e){
+  set_pop_up(e);
+  console.log(pop_up)
+}
+
+const profile_ = (
+  <ul className="profile absolute right-4 top-0 text-white p-4 flex flex-col justify-between " style={{background : "#215d4f"}}>
+    <li className="ite p-1">View Profile</li>
+    <li className="ite p-1">Setting</li>
+    <li className="ite p-1">Logout</li>
+  </ul>
+)
   return (
     <>
-      <div className="navbar  px-5 md:px-10 sticky top-0 w-full  items-center justify-between pt-2 z-50 flex flex-wrap bg-white">
+      <div className="navbar  px-5 md:px-10 sticky top-0 w-full hidden lg:flex  items-center justify-between flex-wrap-reverse pt-2 z-50 lg:flex-wrap bg-white">
         {/* left portion of navbar  */}
         <div className="left border-1  w-auto md:w-1/5 ">
           <img src={logo} alt="logo" className="h-9 hover:cursor-pointer"></img>
         </div>
 
         {/* center portion of navbar  */}
-        <div className="center w-2/4 hidden md:flex">
+        <div className="center w-2/4 hidden lg:flex">
           <ul className="nav-list flex justify-between items-center  w-full ">
             <li
               className="list-item hover:cursor-pointer opacity-50 font-normal"
@@ -95,14 +111,14 @@ const Navbar = () => {
 
         {/* right portion of navbar  */}
         {!login ? (
-          <div className="right w-1/5 hidden md:flex  justify-end">
+          <div className="right w-1/5 hidden lg:flex  justify-end">
             <div className="h-full">
               <a className=" hover:cursor-pointer login h-full">Login</a>
             </div>
             <button className="signup py-1 px-3.5 ml-5">Sign Up</button>
           </div>
         ) : (
-          <div className="right w-1/5 hidden md:flex  justify-end gap-7 items-center ">
+          <div className="right w-1/5 hidden lg:flex  justify-end gap-7 items-center ">
             <div className="relative">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -240,6 +256,55 @@ const Navbar = () => {
             </ul>
           </div>
         )}
+      </div>
+
+      <div className="flex lg:hidden flex-row flex-nowrap mt-2 mx-4 justify-between">
+        {/* left portion hamburger menu*/}
+        <div className="left flex items-center">
+          <Menu color="darkgreen" />
+        </div>
+
+        {/* center logo image  */}
+        <div className=" border-1  w-auto md:w-1/5 flex items-center">
+          <img src={logo} alt="logo" className="h-7 sm:h-9 hover:cursor-pointer" />
+        </div>
+
+        {/* right side profile image and notification icon  */}
+        <div className="right w-1/5 flex lg:hidden  justify-end gap-2 sm:gap-7 items-center ">
+            <div className="relative">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="12"
+                height="13"
+                viewBox="0 0 12 13"
+                fill="none"
+                className="notification-circle" >
+                <circle cx="6" cy="6.98535" r="6" fill="#215D4F" 
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="22"
+                height="27"
+                viewBox="0 0 22 27"
+                fill="none"
+              >
+                <path
+                  d="M10.6151 26.567C9.8853 26.567 9.26034 26.307 8.7402 25.7868C8.22006 25.2667 7.96043 24.6422 7.96132 23.9133H13.2689C13.2689 24.643 13.0088 25.268 12.4887 25.7881C11.9685 26.3083 11.344 26.5679 10.6151 26.567ZM0 22.5864V19.9326H2.65377V10.6444C2.65377 8.76464 3.2177 7.12239 4.34555 5.71766C5.47341 4.31293 6.89981 3.38986 8.62476 2.94845V2.01963C8.62476 1.46676 8.81849 0.9966 9.20594 0.609149C9.59339 0.221698 10.0631 0.0284153 10.6151 0.0292999C11.168 0.0292999 11.6381 0.223025 12.0256 0.610476C12.413 0.997927 12.6063 1.46764 12.6054 2.01963V2.94845C13.0256 3.03691 13.4294 3.16429 13.8169 3.33059C14.2043 3.4969 14.5524 3.69018 14.8611 3.91044C14.2198 4.68446 13.739 5.52482 13.4188 6.43153C13.0986 7.33823 12.938 8.30023 12.9371 9.31751C12.9371 11.1973 13.4515 12.8506 14.4803 14.2774C15.5091 15.7043 16.8745 16.749 18.5764 17.4115V19.9326H21.2302V22.5864H0Z"
+                  fill="#215D4F"
+                  onClick={()=>{
+                    pop_up==="notification"? hadler_popup(""):hadler_popup("notification") ;
+                  }}
+                />
+              </svg>
+            </div>
+            <img src={profile} alt="profile" onClick={()=>{
+             pop_up==="profile"? hadler_popup(""):hadler_popup("profile") ;
+            }}/>
+          </div>
+      </div>
+      <div className="relative">
+        {pop_up==="profile" && profile_}
       </div>
     </>
   );
