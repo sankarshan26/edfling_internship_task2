1d270b26c3bdbca6b243a6d18d7dd6c1e2b11b05
Sankarshan Mishra <100289130+sankarshan26@users.noreply.github.com>
2023-09-02 11:25:14 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import './App.css';
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
@@ -6,18 +7,22 @@ import Account from "./Components/Account";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import Desktop67 from "./Components/Desktop67";
+import MyClassroom from "./Components/MyClassroom/MyClassroom";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 function App() {
   const [isopn, setIsopn] = React.useState(false);
   function handle_isopn(){
     setIsopn(!isopn);
-    // console.log(isopn);
   }
   return (
     
     <Provider store={store}>
-    <Navbar handle_isopn={handle_isopn}/>
-     <Desktop67 isopn={isopn} handle_isopn={handle_isopn} />
+      
+    <Navbar  handle_isopn={handle_isopn}/>
+    {/* <MyClassroom isopn={isopn} handle_isopn={handle_isopn}/> */}
+     {/* <Desktop67 isopn={isopn} handle_isopn={handle_isopn} /> */}
+     <Dashboard isopn={isopn} handle_isopn={handle_isopn}/>
     <Footer />
     </Provider>
   );
